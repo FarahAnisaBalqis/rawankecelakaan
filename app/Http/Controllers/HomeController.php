15ab23090f4fdf6de2220a111d8d56a24a2425ed
unknown <ridharra2@gmail.com>
2022-02-13 13:58:09 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -66,7 +66,6 @@ class HomeController extends Controller
         $grafik2 = HalamanData2::select(DB::raw('YEAR(waktu) year'), DB::raw('count(id) as sum'))
             ->groupBy('year')
             ->get();
-            dd($grafik2);
         $tahun = [];
         $jumlah = [];
         $id = 0;
